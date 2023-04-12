d42dcb0b954bfea12778acd84a1ce3d76033caf8
SilverGreen93 <mihai.vasiliu.93@gmail.com>
2023-04-12 15:26:13 +0300

    Add sample software

---
+++ Software/TrainSim_RailDriverdll_Sample/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace TrainSim_RailDriverdll_Sample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.BtnGetLocoName = new System.Windows.Forms.Button();
+            this.LblLocoName = new System.Windows.Forms.Label();
+            this.BtnGetControllerList = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LblID = new System.Windows.Forms.Label();
+            this.LblDescription = new System.Windows.Forms.Label();
+            this.LblMin = new System.Windows.Forms.Label();
+            this.LblMax = new System.Windows.Forms.Label();
+            this.LblCurrent = new System.Windows.Forms.Label();
+            this.TxtValue = new System.Windows.Forms.TextBox();
+            this.TxtControlID = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnRefresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 24);
+            this.button1.TabIndex = 29;
+            this.button1.Text = "SetRailDriverConnected";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BtnGetLocoName
+            // 
+            this.BtnGetLocoName.Location = new System.Drawing.Point(12, 42);
+            this.BtnGetLocoName.Name = "BtnGetLocoName";
+            this.BtnGetLocoName.Size = new System.Drawing.Size(151, 26);
+            this.BtnGetLocoName.TabIndex = 45;
+            this.BtnGetLocoName.Text = "Get Loco Name";
+            this.BtnGetLocoName.UseVisualStyleBackColor = true;
+            this.BtnGetLocoName.Click += new System.EventHandler(this.BtnGetLocoName_Click);
+            // 
+            // LblLocoName
+            // 
+            this.LblLocoName.Location = new System.Drawing.Point(169, 43);
+            this.LblLocoName.Name = "LblLocoName";
+            this.LblLocoName.Size = new System.Drawing.Size(301, 36);
+            this.LblLocoName.TabIndex = 46;
+            this.LblLocoName.Text = "Loco name";
+            // 
+            // BtnGetControllerList
+            // 
+            this.BtnGetControllerList.Location = new System.Drawing.Point(12, 74);
+            this.BtnGetControllerList.Name = "BtnGetControllerList";
+            this.BtnGetControllerList.Size = new System.Drawing.Size(151, 23);
+            this.BtnGetControllerList.TabIndex = 47;
+            this.BtnGetControllerList.Text = "Get Controller List";
+            this.BtnGetControllerList.UseVisualStyleBackColor = true;
+            this.BtnGetControllerList.Click += new System.EventHandler(this.BtnGetControllerList_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(169, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(286, 13);
+            this.label1.TabIndex = 50;
+            this.label1.Text = "Gets the controller IDs and their min, max and current value";
+            // 
+            // LblID
+            // 
+            this.LblID.AutoSize = true;
+            this.LblID.Location = new System.Drawing.Point(12, 100);
+            this.LblID.Name = "LblID";
+            this.LblID.Size = new System.Drawing.Size(18, 13);
+            this.LblID.TabIndex = 51;
+            this.LblID.Text = "ID";
+            // 
+            // LblDescription
+            // 
+            this.LblDescription.AutoSize = true;
+            this.LblDescription.Location = new System.Drawing.Point(48, 100);
+            this.LblDescription.Name = "LblDescription";
+            this.LblDescription.Size = new System.Drawing.Size(60, 13);
+            this.LblDescription.TabIndex = 52;
+            this.LblDescription.Text = "Description";
+            // 
+            // LblMin
+            // 
+            this.LblMin.AutoSize = true;
+            this.LblMin.Location = new System.Drawing.Point(245, 100);
+            this.LblMin.Name = "LblMin";
+            this.LblMin.Size = new System.Drawing.Size(24, 13);
+            this.LblMin.TabIndex = 53;
+            this.LblMin.Text = "Min";
+            // 
+            // LblMax
+            // 
+            this.LblMax.AutoSize = true;
+            this.LblMax.Location = new System.Drawing.Point(329, 100);
+            this.LblMax.Name = "LblMax";
+            this.LblMax.Size = new System.Drawing.Size(27, 13);
+            this.LblMax.TabIndex = 54;
+            this.LblMax.Text = "Max";
+            // 
+            // LblCurrent
+            // 
+            this.LblCurrent.AutoSize = true;
+            this.LblCurrent.Location = new System.Drawing.Point(409, 100);
+            this.LblCurrent.Name = "LblCurrent";
+            this.LblCurrent.Size = new System.Drawing.Size(41, 13);
+            this.LblCurrent.TabIndex = 55;
+            this.LblCurrent.Text = "Current";
+            // 
+            // TxtValue
+            // 
+            this.TxtValue.Location = new System.Drawing.Point(606, 40);
+            this.TxtValue.Margin = new System.Windows.Forms.Padding(2);
+            this.TxtValue.Name = "TxtValue";
+            this.TxtValue.Size = new System.Drawing.Size(47, 20);
+            this.TxtValue.TabIndex = 58;
+            this.TxtValue.Text = "1";
+            // 
+            // TxtControlID
+            // 
+            this.TxtControlID.Location = new System.Drawing.Point(555, 40);
+            this.TxtControlID.Margin = new System.Windows.Forms.Padding(2);
+            this.TxtControlID.Name = "TxtControlID";
+            this.TxtControlID.Size = new System.Drawing.Size(47, 20);
+            this.TxtControlID.TabIndex = 57;
+            this.TxtControlID.Text = "11";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(481, 37);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(69, 25);
+            this.button3.TabIndex = 56;
+            this.button3.Text = "SetVal";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(603, 25);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(34, 13);
+            this.label12.TabIndex = 289;
+            this.label12.Text = "Value";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(552, 25);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(18, 13);
+            this.label11.TabIndex = 288;
+            this.label11.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(658, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 13);
+            this.label2.TabIndex = 290;
+            this.label2.Text = "Sets the value and refresh list";
+            // 
+            // BtnRefresh
+            // 
+            this.BtnRefresh.Location = new System.Drawing.Point(476, 74);
+            this.BtnRefresh.Name = "BtnRefresh";
+            this.BtnRefresh.Size = new System.Drawing.Size(126, 23);
+            this.BtnRefresh.TabIndex = 291;
+            this.BtnRefresh.Text = "Refresh";
+            this.BtnRefresh.UseVisualStyleBackColor = true;
+            this.BtnRefresh.Click += new System.EventHandler(this.BtnRefresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(833, 862);
+            this.Controls.Add(this.BtnRefresh);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.TxtValue);
+            this.Controls.Add(this.TxtControlID);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.LblCurrent);
+            this.Controls.Add(this.LblMax);
+            this.Controls.Add(this.LblMin);
+            this.Controls.Add(this.LblDescription);
+            this.Controls.Add(this.LblID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnGetControllerList);
+            this.Controls.Add(this.LblLocoName);
+            this.Controls.Add(this.BtnGetLocoName);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Train Sim 2015 Raildriver.dll Sample";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BtnGetLocoName;
+        private System.Windows.Forms.Label LblLocoName;
+        private System.Windows.Forms.Button BtnGetControllerList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LblID;
+        private System.Windows.Forms.Label LblDescription;
+        private System.Windows.Forms.Label LblMin;
+        private System.Windows.Forms.Label LblMax;
+        private System.Windows.Forms.Label LblCurrent;
+        private System.Windows.Forms.TextBox TxtValue;
+        private System.Windows.Forms.TextBox TxtControlID;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnRefresh;
+    }
+}
+
---
+++ Software/TrainSim_RailDriverdll_Sample/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;  //in order to use DllImport 
+
+namespace TrainSim_RailDriverdll_Sample
+{
+    public partial class Form1 : Form
+    {
+        List<int> ids;
+        List<string> descriptions;
+        List<float> mins;
+        List<float> maxs;
+        List<float> currents;
+
+        Label[] lblids = new Label[500]; //?? max number of control ids ??
+        Label[] lbldescriptions = new Label[500];
+        Label[] lblmins = new Label[500];
+        Label[] lblmaxs = new Label[500];
+        Label[] lblcurrents = new Label[500];
+
+        //RailDriver.dll import
+        //string Locationdll = "C:\\Program Files\\Steam\\steamapps\\common\\railworks\\plugins\\RailDriver.dll";
+        //Note add <PlatformTarget>x86</PlatformTarget> to the .csproj file!!
+
+        [DllImport("C:\\Program Files (x86)\\Steam\\steamapps\\common\\railworks\\plugins\\RailDriver.dll")]
+        static extern void SetRailDriverConnected(bool isConnected);
+
+        [DllImport("C:\\Program Files (x86)\\Steam\\steamapps\\common\\railworks\\plugins\\RailDriver.dll")]
+        private static extern bool GetRailSimConnected();
+
+        [DllImport("C:\\Program Files (x86)\\Steam\\steamapps\\common\\railworks\\plugins\\RailDriver.dll")]
+        static extern void SetRailSimValue(int controlID, float value);
+
+        [DllImport("C:\\Program Files (x86)\\Steam\\steamapps\\common\\railworks\\plugins\\RailDriver.dll")]
+        static extern float GetRailSimValue(int controlID, int type);
+
+        [DllImport("C:\\Program Files (x86)\\Steam\\steamapps\\common\\railworks\\plugins\\RailDriver.dll")]
+        static extern Boolean GetRailSimLocoChanged();
+
+        [DllImport("C:\\Program Files (x86)\\Steam\\steamapps\\common\\railworks\\plugins\\RailDriver.dll")]
+        static extern Boolean GetRailSimCombinedThrottleBrake();
+
+        [DllImport("C:\\Program Files (x86)\\Steam\\steamapps\\common\\railworks\\plugins\\RailDriver.dll")]
+        static extern string GetLocoName();
+
+        [DllImport("C:\\Program Files (x86)\\Steam\\steamapps\\common\\railworks\\plugins\\RailDriver.dll")]
+        static extern string GetControllerList();
+
+        [DllImport("C:\\Program Files (x86)\\Steam\\steamapps\\common\\railworks\\plugins\\RailDriver.dll")]
+        static extern float GetControllerValue(int controlID, int type);
+
+        [DllImport("C:\\Program Files (x86)\\Steam\\steamapps\\common\\railworks\\plugins\\RailDriver.dll")]
+        static extern void SetControllerValue(int controlID, float value);
+        
+        public Form1()
+        {
+            InitializeComponent();
+            ids = new List<int>();
+            descriptions = new List<string>();
+            maxs = new List<float>();
+            mins = new List<float>();
+            currents = new List<float>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SetRailDriverConnected(true);
+        }
+
+        private void BtnGetLocoName_Click(object sender, EventArgs e)
+        {
+            LblLocoName.Text = GetLocoName();
+            
+        }
+
+        private void BtnGetControllerList_Click(object sender, EventArgs e)
+        {
+            string controllerlist = GetControllerList();
+            string delimit = "::";
+            string temp = controllerlist;
+            int cnt1 = 0;
+            while (temp.Length > 0)
+            {
+                int pos = temp.IndexOf(delimit);
+                if (pos != -1)
+                {
+                    temp = temp.Remove(0, pos + 2);
+                    cnt1++;
+                }
+                else break;
+            }
+            //number of controllers cnt1+1
+            //int rowCount = dataGridView1.Rows.Count;
+            //while (dataGridView1.Rows.Count > 1)
+            //{
+            //    dataGridView1.Rows.RemoveAt(0);
+            //}
+            ids.Clear();
+            mins.Clear();
+            maxs.Clear();
+            descriptions.Clear();
+            currents.Clear();
+            int cnt = 0;
+            while (controllerlist.Length > 0)
+            {
+                int pos = controllerlist.IndexOf(delimit);
+                if (pos != -1)
+                {
+                    //dataGridView1.Rows.Add();
+                    //dataGridView1[0, cnt].Value = cnt;
+                    //dataGridView1[1, cnt].Value = controllerlist.Substring(0, pos);
+                    ////listBox2.Items.Add(cnt.ToString() + "=" + controllerlist.Substring(0, pos));
+                    descriptions.Add(controllerlist.Substring(0, pos));
+                    controllerlist = controllerlist.Remove(0, pos + 2);
+                    ////get vals
+                    //int cid = System.Convert.ToInt32(cnt);
+                    float retval2 = GetControllerValue(cnt, 0);
+                    currents.Add(retval2);
+                    //dataGridView1[4, cnt].Value= retval2.ToString();
+                    retval2 = GetControllerValue(cnt, 1);
+                    mins.Add(retval2);
+                    //dataGridView1[2, cnt].Value=retval2.ToString();
+                    retval2 = GetControllerValue(cnt, 2);
+                    maxs.Add(retval2);
+                    //dataGridView1[3, cnt].Value = retval2.ToString();
+                    ids.Add(cnt);
+
+                    cnt++;
+                }
+                else break;
+            }
+           //add last one
+            descriptions.Add(controllerlist.Trim());
+            float retval3 = GetControllerValue(cnt, 0);
+            currents.Add(retval3);
+            //dataGridView1[4, cnt].Value= retval2.ToString();
+            retval3 = GetControllerValue(cnt, 1);
+            mins.Add(retval3);
+            //dataGridView1[2, cnt].Value=retval2.ToString();
+            retval3 = GetControllerValue(cnt, 2);
+            maxs.Add(retval3);
+            ids.Add(cnt);
+
+
+
+            int top = BtnGetControllerList.Top + BtnGetControllerList.Height +30;
+            for (int i = 0; i < ids.Count; i++)
+            {
+                lblids[i] = new Label();
+                lbldescriptions[i] = new Label();
+                lblmaxs[i] = new Label();
+                lblmins[i] = new Label();
+                lblcurrents[i] = new Label();
+                
+                lblids[i].Text = ids[i].ToString();
+                lbldescriptions[i].Text = descriptions[i];
+                lblmins[i].Text = mins[i].ToString();
+                lblmaxs[i].Text = maxs[i].ToString();
+                lblcurrents[i].Text = currents[i].ToString();
+
+                int w1 = 25;
+                int w2 = 175;
+                int w3 = 65;
+                int w4 = 65;
+                int w5 = 80;
+
+                lblids[i].Width = w1;
+                lbldescriptions[i].Width = w2;
+                lblmins[i].Width = w3;
+                lblmaxs[i].Width = w4;
+                lblcurrents[i].Width = w5;
+
+                lblids[i].Height = 13;
+                lbldescriptions[i].Height = 13;
+                lblmins[i].Height = 13;
+                lblmaxs[i].Height = 13;
+                lblcurrents[i].Height = 13;
+
+                lblids[i].Left = BtnGetControllerList.Left;
+                lbldescriptions[i].Left = lblids[i].Left + w1;
+                lblmins[i].Left = lblids[i].Left + w1+w2;
+                lblmaxs[i].Left = lblids[i].Left + w1+w2+w3;
+                lblcurrents[i].Left = lblids[i].Left + w1+w2+w3+w4;
+
+                if (i == 0)
+                {
+                    lblids[i].Top = top;
+                }
+                else
+                {
+                    lblids[i].Top = lblids[i - 1].Top + 12;
+                }
+                lbldescriptions[i].Top = lblids[i].Top;
+                lblmins[i].Top = lblids[i].Top;
+                lblmaxs[i].Top = lblids[i].Top;
+                lblcurrents[i].Top = lblids[i].Top;
+
+                lblids[i].Visible = true;
+                lbldescriptions[i].Visible = true;
+                lblmins[i].Visible = true;
+                lblmaxs[i].Visible = true;
+                lblcurrents[i].Visible = true;
+
+                this.Controls.Add(lblids[i]);
+                this.Controls.Add(lbldescriptions[i]);
+                this.Controls.Add(lblmins[i]);
+                this.Controls.Add(lblmaxs[i]);
+                this.Controls.Add(lblcurrents[i]);
+            }
+            LblID.Left = lblids[0].Left;
+            LblDescription.Left = lbldescriptions[0].Left;
+            LblMin.Left = lblmins[0].Left;
+            LblMax.Left = lblmaxs[0].Left;
+            LblCurrent.Left = lblcurrents[0].Left;
+            int stop = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int cid = System.Convert.ToInt32(TxtControlID.Text);
+            float val = (float)System.Convert.ToDouble(TxtValue.Text);
+            //SetRailSimValue(cid, val); //doesn't work anymore
+            SetControllerValue(cid, val);
+            //refresh the list 
+            RefreshVals();
+        }
+        private void RefreshVals()
+        {
+            int thiscnt = lblids.Count();
+            for (int i = 0; i < lblids.Count(); i++)
+            {
+                if (lblids[i]!=null)
+                {
+                    float retval2 = GetControllerValue(i, 0);
+                    lblcurrents[i].Text = retval2.ToString();
+                }
+            }
+        }
+
+        private void BtnRefresh_Click(object sender, EventArgs e)
+        {
+            RefreshVals();
+        }
+
+
+
+    }
+}
